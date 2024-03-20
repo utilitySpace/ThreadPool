f9d490bffc91dd3b9806f5a3fa614d0d7b8429ff
zhouleigang <meixi@live.cn>
2024-03-20 10:09:24 +0800

    use aotmic_bool type

--- ThreadPool.h
+++ ThreadPool.h
@@ -9,6 +9,7 @@
 #include <condition_variable>
 #include <future>
 #include <functional>
+#include <atomic>
 #include <stdexcept>
 
 class ThreadPool {
@@ -27,7 +28,7 @@ private:
     // synchronization
     std::mutex queue_mutex;
     std::condition_variable condition;
-    bool stop;
+    std::atomic_bool stop;
 };
  
 // the constructor just launches some amount of workers
